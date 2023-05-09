8326b736834a55cb231a13e77fbc05070a623549
Manik <tonysins43@gmail.com>
2023-05-09 23:15:17 +0600

    initail project setup

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element:<Main></Main>,
+      children:[
+        {
+            path:"/",
+        }
+      ]
+    },
+  ]);
+
+  export default router;
